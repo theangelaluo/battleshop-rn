81a0f6aa9b7ffc1d243848a5a9f27c257df644ed
Angela <theangelaluo@gmail.com>
2018-11-29 04:20:20 -0800

    styling changes

--- App/App.js
+++ App/App.js
@@ -14,6 +14,10 @@ import {
   FlatList,
 } from 'react-native';
 
+import { Icon } from 'react-native-elements';
+
+import TimePicker from 'react-native-simple-time-picker';
+
 //var FBLoginButton = require('./FBLoginButton');
 
 import { createBottomTabNavigator,  createStackNavigator, createAppContainer } from 'react-navigation';
@@ -116,16 +120,16 @@ class GroupOrSolo extends React.Component {
   render() {
     const { navigation } = this.props;
     return (
-      <View style={styles.container}>
-          <View style={{display: 'flex', flexDirection: 'column', marginTop: 10}}>
-            <TouchableOpacity onPress={() => this.toChooseOpponents()} style={[styles.button, {backgroundColor: "#f3c677", borderRadius: 2}]}>
-              <Text style={{paddingRight: 15, paddingLeft: 15, textAlign: 'center', fontSize: 100, color: 'black'}}>Group</Text>
+          <View style={{backgroundColor: '#F9564F', width: '100%', height: '100%', display: 'flex', flexDirection: 'column', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <TouchableOpacity onPress={() => this.toChooseOpponents()} style={[styles.button, styles.buttonShadow, {display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly', width: '90%', height: '30%', backgroundColor: "#f3c677", borderRadius: 15, marginBottom: 25,}]}>
+              <Text style={{fontSize: 60, color: 'black'}}>Group</Text>
+              <Icon name='arrow-forward' size={40} />
               </TouchableOpacity>
-              <TouchableOpacity onPress={() => this.toHuntOrSave()} style={[styles.button, {backgroundColor: '#f3c677', borderRadius: 2}]}>
-              <Text style={{paddingRight: 15, paddingLeft: 15, textAlign: 'center', fontSize: 100, color: 'black'}}>Solo</Text>
-              </TouchableOpacity>
+            <TouchableOpacity onPress={() => this.toHuntOrSave()} style={[styles.button, styles.buttonShadow, {display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly', width: '90%', height: '30%', backgroundColor: '#f3c677', borderRadius: 15, marginTop: 25}]}>
+              <Text style={{textAlign: 'center', fontSize: 60, color: 'black'}}>Solo</Text>
+              <Icon name='arrow-forward' size={40} />
+            </TouchableOpacity>
           </View>
-      </View>
     )
   }
 }
@@ -296,15 +300,15 @@ class HuntOrSave extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={{display: 'flex', flexDirection: 'column', marginTop: 10}}>
-            <TouchableOpacity onPress={() => this.toHunt()} style={[styles.button, {backgroundColor: "#f3c677", borderRadius: 2}]}>
-              <Text style={{paddingRight: 15, paddingLeft: 15, textAlign: 'center', fontSize: 100, color: 'black'}}>Hunt</Text>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={() => this.toSave()} style={[styles.button, {backgroundColor: '#f3c677', borderRadius: 2}]}>
-              <Text style={{paddingRight: 15, paddingLeft: 15, textAlign: 'center', fontSize: 100, color: 'black'}}>Save</Text>
-              </TouchableOpacity>
-          </View>
+      <View style={{backgroundColor: '#F9564F', width: '100%', height: '100%', display: 'flex', flexDirection: 'column', flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <TouchableOpacity onPress={() => this.toHunt()} style={[styles.button, styles.buttonShadow, {display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly', width: '90%', height: '30%', backgroundColor: "#f3c677", borderRadius: 15, marginBottom: 25,}]}>
+          <Text style={{fontSize: 60, color: 'black'}}>Hunt</Text>
+          <Icon name='arrow-forward' size={40} />
+          </TouchableOpacity>
+        <TouchableOpacity onPress={() => this.toSave()} style={[styles.button, styles.buttonShadow, {display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly', width: '90%', height: '30%', backgroundColor: '#f3c677', borderRadius: 15, marginTop: 25}]}>
+          <Text style={{textAlign: 'center', fontSize: 60, color: 'black'}}>Save</Text>
+          <Icon name='arrow-forward' size={40} />
+        </TouchableOpacity>
       </View>
     )
   }
@@ -496,13 +500,9 @@ itemButton: {
   borderRadius: 15
  },
   button: {
-    alignSelf: 'stretch',
-    paddingTop: 10,
-    paddingBottom: 10,
-    marginTop: 10,
-    marginLeft: 5,
-    marginRight: 5,
-    borderRadius: 5
+//alignSelf: 'stretch',
+    padding: 10,
+    margin: 10
   },
   yellowContainer: {
     flex: 0,
@@ -526,5 +526,19 @@ itemButton: {
     height: 70,
     width: 210,
     fontSize: 30,
+  },
+  buttonShadow: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#fff',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
   },
 });
